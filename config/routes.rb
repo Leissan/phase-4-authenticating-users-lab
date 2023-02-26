04f60663afa54c6fb94f3b340859aca3087c2f32
Leissan Khan <leissan.khan@gmail.com>
2023-02-26 14:17:14 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/login', to: 'sessions#create'
   get "/me", to: "users#show"
